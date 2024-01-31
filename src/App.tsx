b4eba6f3920652c91eb6c83c04b3616e913f7d49
“lichtle” <leticia.negreiros@outlook.com>
2024-01-31 16:40:21 -0300

    chore: montagem da página inicial no componente App (home)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { GlobalStyle } from "./styles";
+
+import Header from "./components/Header";
+import ProductsList from "./components/ProductsList";
+import Footer from "./components/Footer";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <ProductsList />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
