9ea0aa2212d014d18ccba40d86d6e9f81d29731d
“lichtle” <leticia.negreiros@outlook.com>
2024-02-02 16:13:52 -0300

    feat: interação com a api adicionada na página inicial (home)

--- src/components/RestaurantsList/index.tsx
+++ src/components/RestaurantsList/index.tsx
@@ -1,29 +1,42 @@
-import { List } from "./styles";
-import Restaurant from "../Restaurant";
+import { useState, useEffect } from "react";
 
-const RestaurantsList = () => (
-  <>
-    <List className="container">
-      <li>
-        <Restaurant />
-      </li>
-      <li>
-        <Restaurant />
-      </li>
-      <li>
-        <Restaurant />
-      </li>
-      <li>
-        <Restaurant />
-      </li>
-      <li>
-        <Restaurant />
-      </li>
-      <li>
-        <Restaurant />
-      </li>
-    </List>
-  </>
-);
+import { List } from "./styles";
+import Restaurant, { RestaurantType } from "../Restaurant";
+
+// type Props = {
+//   restaurants: RestaurantType[];
+// };
+
+const RestaurantsList = () => {
+  const [restaurantsList, setRestaurantsList] = useState<RestaurantType[]>([]);
+
+  useEffect(() => {
+    fetch("https://fake-api-tau.vercel.app/api/efood/restaurantes")
+      .then((answer) => answer.json())
+      .then((answer) => setRestaurantsList(answer)); //
+  }, []);
+
+  return (
+    <>
+      <List className="container">
+        {restaurantsList.map((restaurant) => {
+          return (
+            <li key={restaurant.id}>
+              <Restaurant
+                id={restaurant.id}
+                titulo={restaurant.titulo}
+                destacado={restaurant.destacado}
+                tipo={restaurant.tipo}
+                avaliacao={restaurant.avaliacao}
+                descricao={restaurant.descricao}
+                capa={restaurant.capa}
+              />
+            </li>
+          );
+        })}
+      </List>
+    </>
+  );
+};
 
 export default RestaurantsList;
