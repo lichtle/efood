7f57f62589995c1b18785cbca83194384c2ff6a2
“lichtle” <leticia.negreiros@outlook.com>
2024-02-05 18:17:31 -0300

    feat: adiciona a contagem de itens no carrinho

--- src/components/DishesHeader/index.tsx
+++ src/components/DishesHeader/index.tsx
@@ -1,19 +1,29 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootReducer } from "../../store";
 
 import { BackgroundContainer } from "./styles";
 
 import logo from "../../assets/logo.png";
 
-const DishesHeader = () => (
-  <BackgroundContainer>
-    <div className="container">
-      <h1>Restaurantes</h1>
-      <Link to="/">
-        <img src={logo} alt="efood" />
-      </Link>
-      <span>0 produto(s) no carrinho</span>
-    </div>
-  </BackgroundContainer>
-);
+const DishesHeader = () => {
+  const { items } = useSelector((state: RootReducer) => state.cart);
+
+  return (
+    <BackgroundContainer>
+      <div className="container">
+        <h1>Restaurantes</h1>
+        <Link to="/">
+          <img src={logo} alt="efood" />
+        </Link>
+        {items.length === 1 ? (
+          <span>1 produto no carrinho</span>
+        ) : (
+          <span>{items.length} produtos no carrinho</span>
+        )}
+      </div>
+    </BackgroundContainer>
+  );
+};
 
 export default DishesHeader;
