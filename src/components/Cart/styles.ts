65d1d9e6e03f7d4d0ab586ca7f3bafee9c8a1e67
“lichtle” <leticia.negreiros@outlook.com>
2024-02-08 19:26:15 -0300

    feat: base para o fluxo de checkout

--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -94,3 +94,50 @@ export const TotalPrice = styled.div`
     justify-content: space-between;
   }
 `;
+
+// Delivery Form
+
+export const SectionTitle = styled.h2`
+  font-size: 16px;
+  font-weight: 700;
+  margin-bottom: 16px;
+  color: ${cores.background};
+`;
+
+export const Input = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  label {
+    margin-bottom: 8px;
+    font-size: 14px;
+    font-weight: 700;
+    color: ${cores.background};
+  }
+
+  input {
+    height: 32px;
+    margin-bottom: 8px;
+    border: none;
+
+    &.error {
+      border: 1px solid red;
+    }
+  }
+`;
+
+export const InputGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  #code {
+    width: 87px;
+  }
+`;
+
+export const ButtonGroup = styled.div`
+  margin-top: 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
