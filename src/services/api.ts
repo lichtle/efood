149a8d69ea592a049049ef8ae7f405621346edbc
“lichtle” <leticia.negreiros@outlook.com>
2024-02-08 19:25:00 -0300

    feat: adiciona o endpoint "purchase" para o método POST

---
+++ src/services/api.ts
@@ -0,0 +1,55 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+type Product = {
+  id: number;
+  price: number;
+};
+
+type PurchasePayload = {
+  products: Product[];
+  delivery: {
+    receiver: string;
+    address: {
+      description: string;
+      city: string;
+      zipCode: string;
+      number: number;
+      complement?: string;
+    };
+  };
+  payment: {
+    card: {
+      name: string;
+      number: string;
+      code: number;
+      expires: {
+        month: number;
+        year: number;
+      };
+    };
+  };
+};
+
+// type PurchaseResponse = {
+//   orderId: string;
+// };
+
+const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://fake-api-tau.vercel.app/api/efood",
+  }),
+  endpoints: (builder) => ({
+    purchase: builder.mutation<any, PurchasePayload>({
+      // Enquanto o query é utilizado para obter dados, o mutation é utilizado para enviar dados
+      query: (body) => ({
+        url: "checkout",
+        method: "POST",
+        body,
+      }),
+    }),
+  }),
+});
+
+export const { usePurchaseMutation } = api;
+
+export default api;
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import cartSlice from "../store/reducers/cart";
 
+import api from "../services/api";
+
 export const store = configureStore({
   reducer: {
     cart: cartSlice,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootReducer = ReturnType<typeof store.getState>;
