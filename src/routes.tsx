90bee1766b4b552027e4c3db726ad9d89ffef625
“lichtle” <leticia.negreiros@outlook.com>
2024-02-13 18:24:21 -0300

    fix: correção na rota home

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import RestaurantDetails from "./pages/RestaurantDetails";
 
 const Rotas = () => (
